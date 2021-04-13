618c7ca17ba9b9c5478c26f575d6f2f8db7a18ae
alexhe101 <hexuanhua101@gmail.com>
2021-04-13 22:56:43 +0800

    加入获取家族成员功能

--- app.py
+++ app.py
@@ -265,6 +265,75 @@ def login():
 
 
 
+@jwt_required()
+@app.route('/family/<string:id>/details',methods=['GET'])
+def get_family_detail(id):
+    '''首先 构建出所有的用户信息dict
+    id最后应该统一化为string 目前使用的测试数据不统一，因此用了多于代码进行处理
+    # dict['id':[dict(person),relation]]
+    # person:
+    # type:dict
+    familyTree[id]:personDict
+    # personDict['name']=string
+    personDict['mates']:list[person]
+    personDict['children']:list[person]
+    '''
+    currentUserId = get_jwt_identity()
+    familyMembersDict = dict()
+    #构建一个由id persondict组成的列表， 并且root是id号即可
+    familyTree = dict()
+    root = None
+    familyMemberQuery = {"families":id}
+    family = mongo.db.family.find_one_or_404({'_id': id})
+    family = Family(entries=family)
+    if  currentUserId == 1 or currentUserId in family.admins:
+        familyMebers = list(mongo.db.person.find(familyMemberQuery))
+        # 以id为key建立索引，方便之后查找
+        for person in familyMebers:
+            #用id为key建立familyMembers字典，属性为(当前角色，当前角色的孩子id，当前角色的matesId)，采用这些信息构建familyTree
+            familyMembersDict[person["_id"]]=[person]
+            childrenQuery = {"from_person": str(person["_id"]), "type": 1}
+            mateQuery = {"from_person": str(person["_id"]),"$or":[{"type":3},{"type":4}]}
+            childrenRelations = list(mongo.db.relation.find(childrenQuery))
+            mateRelations = list(mongo.db.relation.find(mateQuery))
+            childIds = [r['to_person'] for r in childrenRelations]
+            mateIds = [r['to_person'] for r in mateRelations]
+            familyMembersDict[person["_id"]].append(childIds)
+            familyMembersDict[person["_id"]].append(mateIds)
+
+            #personDict为最终输出时需要用到的数据
+            personDict= dict()
+            personDict['name'] = person['name']
+            personDict['mates'] = []
+            personDict['children'] = []
+            personDict['image_url'] = ""
+            familyTree[person["_id"]] = personDict
+        for k,v in familyTree.items():
+            if root==None:
+                root = k
+            childIds = familyMembersDict[k][1]
+            for id in childIds:
+                # 如果根节点是当前节点的孩子，那么当前节点是根节点
+                if root==id:
+                    root = k
+                familyTree[k]['children'].append(familyTree[id])
+            mateIds = familyMembersDict[k][2]
+            for mateid in mateIds:
+                query = {"_id":mateid}
+                mate = mongo.db.person.find_one(query)
+                mateDict = dict()
+                mateDict['name'] = mate['name']
+                mateDict['image_url'] = ""
+                familyTree[k]['mates'].append(mateDict)
+        print(familyMembersDict)
+        return familyTree[root]
+    else:
+        abort(403)
+
+
+
+
+
 
 if __name__ == '__main__':
     app.debug = True
