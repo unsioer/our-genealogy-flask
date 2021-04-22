fa0f5b377eb7d08d31dca7ed60f1ec2e0a062636
alexhe101 <hexuanhua101@gmail.com>
2021-04-22 08:10:30 +0800

    增加点赞功能实现

--- our-genealogy/blueprints/article.py
+++ our-genealogy/blueprints/article.py
@@ -3,11 +3,11 @@ from flask import Blueprint, request, jsonify
 import json
 from flask_jwt_extended import (
     jwt_required, create_access_token,
-    get_jwt_identity
+    get_jwt_identity, get_jwt
 )
 
 from models.article import Article
-from utils.utils import generateID
+from utils.utils import generateID,currentTime
 
 from utils.ApiError import *
 from extensions import mongo,redis_client
@@ -15,6 +15,7 @@ from schema.schemas import(
     check_data,
     PersonSchema
 )
+from utils.authCheck import *
 
 articles_bp = Blueprint("articles",__name__)
 
@@ -47,6 +48,7 @@ def get_article(id):
 
 @articles_bp.route('/articles/<string:id>/view',methods=['PUT'])
 def add_view(id):
+    # 似乎也要做权限校验 有点麻烦 需要访问数据库
     ip = request.remote_addr
     key = ip+":"+id
     if not redis_client.exists(key):
@@ -55,4 +57,49 @@ def add_view(id):
         article_key = "viewCount:"+id
         redis_client.incr(article_key) #redis中增加浏览数
         #之后定时自动写回
-        return "OK"
+        return "ok"
+
+@articles_bp.route('/article/<string:id>/like',methods=['PUT'])
+@jwt_required()
+def add_like(id):
+    user_id = get_jwt_identity()
+    is_admin = get_jwt()['is_admin']
+    like_query={"$and":[{"user_id":user_id},{"article_id":id}]}
+    like = mongo.db.like.find_one(like_query)
+    if like!=None:
+        raise ApiError(REPETITIVE_OPERATION)
+    query = {"_id":id}
+    articles = mongo.db.article.find_one_or_404({'_id': id})
+    article = Article(entries=articles)
+    if check_article_like_auth(article,is_admin,user_id):
+        update_data = { "$inc": { "like_num":1} }
+        mongo.db.article.update_one(query,update_data)
+        like = dict()
+        like['_id'] = generateID()
+        like['user_id']=user_id
+        like['article_id'] = article.id
+        like['time']=currentTime()
+        mongo.db.like.insert_one(like)
+        return "ok"
+    raise ApiError(NO_AUTH,403)
+
+@articles_bp.route('/article/<string:id>/like',methods=['DELETE'])
+@jwt_required()
+def cancel_like(id):
+    user_id = get_jwt_identity()
+    is_admin = get_jwt()['is_admin']
+    like_query={"$and":[{"user_id":user_id},{"article_id":id}]}
+    query = {"_id":id}
+    articles = mongo.db.article.find_one_or_404({'_id': id})
+    article = Article(entries=articles)
+    if check_article_like_auth(article,is_admin,user_id):
+        count = mongo.db.like.delete_one(like_query).deleted_count
+        print(count)
+        if count!=0:
+            update_data = { "$inc": { "like_num":-1} }
+            mongo.db.article.update_one(query,update_data)
+            return "ok"
+        else:
+            raise ApiError(NOT_FOUND,404)
+    raise ApiError(NO_AUTH,403)
+
--- our-genealogy/utils/authCheck.py
+++ our-genealogy/utils/authCheck.py
@@ -1,3 +1,4 @@
+from models.article import Article
 from models.family import Family
 
 
@@ -24,3 +25,14 @@ def check_family_read_auth(family: Family, current_user_id, is_admin):
         return True
     if current_user_id in family.admins or current_user_id == family.created_by or current_user_id in family.read_admins:
         return True
+
+def check_article_like_auth(article:Article,is_admin=None,current_user_id=None):
+    if article.access_level==0:
+        return True
+    if is_admin:
+        return True
+    if article.user_id==current_user_id:
+        return True
+    return False
+
+
